0c296a27152b44410d3dc5d2f90775c5bf1f845a
Ryan Izac <io.ryan@outlook.com>
2023-01-30 13:07:47 -0300

    fix: add margin top for form in view container

--- src/components/form/Form/styles.ts
+++ src/components/form/Form/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  margin: auto 0;
   gap: 16px;
 
   h2 {
--- src/views/SignupLocallyView/styles.ts
+++ src/views/SignupLocallyView/styles.ts
@@ -1,4 +1,8 @@
 import { View } from '@/components/template/View'
 import styled from 'styled-components'
 
-export const Container = styled(View)``
+export const Container = styled(View)`
+  form {
+    margin-top: 64px;
+  }
+`
