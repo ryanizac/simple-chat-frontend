419546a8e8a7ed6ac8c12faec4f8ec5a6958e0c1
Ryan Izac <io.ryan@outlook.com>
2023-01-30 23:11:40 -0300

    fix: change style buttons from anchor to button

--- src/components/buttons/PrimaryButton/styles.ts
+++ src/components/buttons/PrimaryButton/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.a`
+export const Container = styled.button`
   text-decoration: none;
   background-color: #0066ff;
   text-align: center;
--- src/components/buttons/SecondaryButton/styles.ts
+++ src/components/buttons/SecondaryButton/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.a`
+export const Container = styled.button`
   text-decoration: none;
   text-align: center;
   width: 100%;
